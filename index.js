8815983e2cdaa170f92c2a8180c6b6d4806d95ec
Sergio Castañeda Valenciano <sergiocasta23@gmail.com>
2020-04-27 21:03:03 +0200

    Subida de los archivos principales

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var who = ['El perro', 'Mi abuela', 'Nuestra tortuga', 'El pájaro del vecino'];
+var action = ['se comió', 'escondió', 'aplastó', 'rompió'];
+var what = ['mis deberes', 'el móvil', 'las llaves', 'los cables'];
+var when = ['antes de clase.', 'justo ahora.', 'mientras hacia el pino.', 'en la hora de la comida.','mientras hacia petit point'];
+
+
+function Excusa() {
+    var quien = who[Math.floor(Math.random() * (who.length))];
+    var accion = action[Math.floor(Math.random() * (action.length))];
+    var que = what[Math.floor(Math.random() * (what.length))];
+    var cuando = when[Math.floor(Math.random() * (when.length))];
+    
+    //return quien +" "+ accion + " " + que + " " + cuando;
+    return quien.concat(" ", accion, " ", que, " ", cuando)
+
+    //return console.log(quien +" "+ accion + " " + que + " " + cuando);
+   
+};
+
+ function onLoad(){
+     document.querySelector('#excusa').innerHTML = Excusa();
+ }
+
+window.onload = onLoad;
+
+
+  
